dfb3bf61f9d603e4b875a7c645ae1776ab5259fc
fnagle <45022675+fnagle@users.noreply.github.com>
2020-09-17 09:30:22 +0800

    Update test_bert.py

--- test_bert.py
+++ test_bert.py
@@ -1,2 +1,12 @@
+import torch
+import Bert
+import os
+from transformers import BertForSequenceClassification, AdamW,BertTokenizer,BertModel
+ # 测试
+if __name__=="__main__":
+    params_dir='model/bert_base_model_test.pkl'
 
- 
+    path='/data/yanghan/Bert_related/bert_base_uncased/'
+    model=BertForSequenceClassification.from_pretrained(path)
+    model.load_state_dict(torch.load(params_dir))
+    Bert.test_model(model)
